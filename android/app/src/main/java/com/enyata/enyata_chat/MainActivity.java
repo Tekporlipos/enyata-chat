52cceb8e0ba10d28e86da784b825d9a7b44c917a
John Dzikunu <56548679+tekpor@users.noreply.github.com>
2023-06-19 19:26:10 +0000

    folder structure and file structure for init code

---
+++ android/app/src/main/java/com/enyata/enyata_chat/MainActivity.java
@@ -0,0 +1,6 @@
+package com.enyata.enyata_chat;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/models/conversation.dart
@@ -0,0 +1,6 @@
+class Conversation {
+  final int id;
+  final String title;
+
+  Conversation({required this.id, required this.title});
+}
